b82a6e21a6d3877acbe75c65d9a3ac8d8a22e53c
myleen12 <myleen.santana12@icloud.com>
2023-01-15 18:11:18 -0800

    added varibles, numnber, letter and speacial chars qw

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // Assignment code here
+var password;
+var uppercase;
+var lowcase;
+var numbers;
+var speacialcharacters;
 
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
+var number = [0,1,2,3,4,5,6,7,8,9];
+var speacialcharacters = ['!','@','#','$','%','^','&','*','(',')','~','<','>'];
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          rd input
-function writePassword(length) {
+function writePassword() {
   var password = generatePassword('');
   var passwordText = document.querySelector("#password");
 
